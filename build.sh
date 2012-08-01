b4ac188be7837eb904efa9c67e0cbc3bd0b9db05
jt1134 <jt1134@gmail.com>
2012-07-31 22:26:31 -0500

    initial

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+DEVICES="fascinatemtd mesmerizemtd showcasemtd"
+. build/envsetup.sh
+
+DoIt() {
+	brunch "$1" || exit 1
+}
+
+Impregnate() {
+	cp out/target/product/"$1"/cm-10-*.zip ~/Dropbox/Public || exit 1
+}
+
+GTFO() {
+	rm -rf out
+}
+
+MakeBabies() {
+	Doit "$1" && Impregnate "$1" && GTFO
+}
+
+GTFO
+if [ ! "$1" ]; then
+	for x in "$DEVICES"
+	do
+		MakeBabies "$x"
+	done
+else
+	MakeBabies "$1"
+fi
+
